6fc27452b89c9aee6846b14c08e98ef1846c4fab
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-09-03 15:26:06 -0300

    orientação de como atualizar a pagina, precisa para o server e iniciar novamente.

--- Secao3/2_retornando_html _atualizacao/index.js
+++ Secao3/2_retornando_html _atualizacao/index.js
@@ -5,7 +5,7 @@ const port = 3000
 const server = http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader('Contenty-type', 'text/html')
-    res.end('<h1>Olá, este é meu primeiro server com HTML!</h1><p>Tentando atualização da página</p>')
+    res.end('<h1>Olá, este é meu primeiro server com HTML!<h1>')
 })
 
 server.listen(port, () => {
