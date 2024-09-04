ccb999f9c8636738e09ad88647e1f54d26de0ff0
Rogerio Cordeiro <rogerio.silva.pb@compasso.com.br>
2024-09-03 23:26:15 -0300

    Implementada a função de realizar deposito na conta.

--- Secao5/5_ACCOUNTS/index.js
+++ Secao5/5_ACCOUNTS/index.js
@@ -24,8 +24,20 @@ function operation() {
         ])
         .then((answer) => {
             const action = answer['action']
+
+
             if (action == 'Criar Conta') {
                 createAccount()
+            } else if (action == 'Consultar Saldo') {
+
+            } else if (action == 'Depositar') {
+                console.log('passou aqui')
+                deposit()
+            } else if (action == 'Sacar') {
+
+            } else if (action == 'Sair') {
+                console.log(chalk.bgBlue('Obrigado por usar o Accounts!'))
+                process.exit()
             }
         })
         .catch((err) => console.log(err))
@@ -33,7 +45,7 @@ function operation() {
 
 // Create an account
 function createAccount() {
-    console.log(chalk.bgGreen.whiteBright('Obrigado por escolher o nosso banco!'))
+    console.log(chalk.bgGreen.white('Obrigado por escolher o nosso banco!'))
     console.log(chalk.green('Defina as opções da sua conta a seguir!'))
     buildAccount()
 }
@@ -68,7 +80,80 @@ function buildAccount() {
             )
 
             console.log(chalk.bgGreen('Parabéns, sua conta foi criada!'))
-
+            operation()
         })
         .catch((err) => console.log(err))
+}
+
+// add an amount to user account
+function deposit() {
+
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Qual o nome da sua conta?'
+        }
+    ])
+        .then((answer) => {
+            const accountName = answer['accountName']
+            if (!checkAccount(accountName)) {
+                return deposit()
+            }
+
+            inquirer.prompt([
+                {
+                    name: 'amount',
+                    message: `Quanto deseja depositar na conta ${accountName}?`
+                },
+            ])
+                .then((answer) => {
+                    const amount = answer['amount']
+
+                    addAmount(accountName, amount)
+                    operation()
+                }).catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
+}
+
+// verify if account exists
+function checkAccount(accountName) {
+    if (!fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(chalk.bgRed.black(`A conta de nome ${accountName} não existe, digite novamente!`))
+        return false
+    }
+
+    return true
+}
+
+function addAmount(accountName, amount) {
+    const accountData = getAccount(accountName)
+
+    if (!amount) {
+        console.log(
+            chalk.bgRed.white('Valor digitado inválido! Refazer operação.')
+        )
+        return deposit()
+    }
+    accountData.balance = parseFloat(amount) + parseFloat(accountData.balance)
+
+    fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        JSON.stringify(accountData),
+        function (err) {
+            console.log(err)
+        }
+    )
+
+    console.log(
+        chalk.green(`Foi depositado R$ ${amount} na conta ${accountName}.`)
+    )
+}
+
+function getAccount(accountName) {
+    const accountJSON = fs.readFileSync(`accounts/${accountName}.json`, {
+        encoding: 'utf8',
+        flag: 'r'
+    })
+    return JSON.parse(accountJSON)
 }
