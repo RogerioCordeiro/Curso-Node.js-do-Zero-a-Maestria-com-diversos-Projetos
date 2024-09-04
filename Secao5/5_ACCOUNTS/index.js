242f7f3394f1751d86674ef204aecbdd57799383
Rogerio Cordeiro <rogerio.silva.pb@compasso.com.br>
2024-09-03 21:51:54 -0300

    Implementada a função do menu principal do sistema Conta bancária.

---
+++ Secao5/5_ACCOUNTS/index.js
@@ -0,0 +1,30 @@
+const chalk = require('chalk')
+const inquirer = require('inquirer')
+
+// modulos internos
+const fs = require('fs')
+
+operation()
+
+function operation() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'O que você deseja fazer?',
+                choices: [
+                    'Criar Conta',
+                    'Consultar Saldo',
+                    'Depositar',
+                    'Sacar',
+                    'Sair',
+                ],
+            },
+        ])
+        .then((answer) => {
+            const action = answer['action']
+            console.log(action)
+        })
+        .catch((err) => console.log(err))
+}
