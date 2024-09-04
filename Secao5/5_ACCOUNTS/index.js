2ce6c1d5f86e41cd743f6e39f91fce13f3201954
Rogerio Cordeiro <rogerio.silva.pb@compasso.com.br>
2024-09-03 22:21:52 -0300

    Implementanda a função que realizar a verificação e criação de conta solicitando o nome para o usuário;

--- Secao5/5_ACCOUNTS/index.js
+++ Secao5/5_ACCOUNTS/index.js
@@ -24,7 +24,51 @@ function operation() {
         ])
         .then((answer) => {
             const action = answer['action']
-            console.log(action)
+            if (action == 'Criar Conta') {
+                createAccount()
+            }
+        })
+        .catch((err) => console.log(err))
+}
+
+// Create an account
+function createAccount() {
+    console.log(chalk.bgGreen.whiteBright('Obrigado por escolher o nosso banco!'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir!'))
+    buildAccount()
+}
+
+function buildAccount() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Digite um nome para a sua conta:'
+        }
+    ])
+        .then((answer) => {
+            const accountName = answer['accountName']
+            console.info(accountName)
+            if (!fs.existsSync('accounts')) {
+                fs.mkdirSync('accounts')
+            }
+            if (fs.existsSync(`accounts/${accountName}.json`)) {
+                console.log(
+                    chalk.bgRed.whiteBright(`O nome de conta ${accountName} já existe, exolha outro nome!`),
+                )
+                buildAccount()
+                return
+            }
+
+            fs.writeFileSync(
+                `accounts/${accountName}.json`,
+                '{"balance":0}',
+                function (err) {
+                    console.log(err)
+                },
+            )
+
+            console.log(chalk.bgGreen('Parabéns, sua conta foi criada!'))
+
         })
         .catch((err) => console.log(err))
 }
